89673bb6fe61c9c35e639d30127147e0027b0d8f
nitinpundir146-beep <nitinpundir146@gmail.com>
2025-11-19 11:43:39 +0000

    fix: added temporary links

--- src/constants/data.jsx
+++ src/constants/data.jsx
@@ -153,7 +153,7 @@ export const skills = [
 export const certificates = [
   {
     title: "Great Learning : Intro to Graphic Design with Photoshop",
-    url: "https://www.mygreatlearning.com/academy/learn/intro-to-graphic-design"
+    url: "#"
   },
   {
     title: "ITRC : Diploma in Computer Applications",
@@ -161,7 +161,7 @@ export const certificates = [
   },
   {
     title: "Great Learning : Canva Essentials: Quick & Easy Design Skills",
-    url: "https://www.canva.com/"
+    url: "#"
   }
 ];
 
